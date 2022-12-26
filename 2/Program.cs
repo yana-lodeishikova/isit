2b6fb0c73cad830a97e282100b53cb429348cc29
Yana Lodeyschikova <kabinet_nachalnika@mail.ru>
2022-12-26 21:38:40 +0500

    program

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,3 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
+using ExpertSystem;
 
-Console.WriteLine("Hello, World!");
+var options = new JsonSerializerOptions { IncludeFields = true, IgnoreReadOnlyFields = true, IgnoreReadOnlyProperties = true };
+options.Converters.Add(new ConditionConverter());
+
+var jsonString = File.ReadAllText("../../../Rules.json");
+var rules = JsonSerializer.Deserialize<Rule[]>(jsonString, options);
+
+var memory = new MemoryComponent(rules);
+var inferenceComponent = new InferenceComponent(memory);
+
+var result = inferenceComponent.GetResult();
+
+if (result is Fact resultFact)
+{
+    Console.WriteLine($"Результат:\n{resultFact.Name}");
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("Результат не определен");
+}
