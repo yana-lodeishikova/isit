8e48c6d7ed7112681a9810cdb887c70b74a96471
Yana Lodeyschikova <kabinet_nachalnika@mail.ru>
2023-01-22 20:26:15 +0500

    examples

--- 5/Form1.Designer.cs
+++ 5/Form1.Designer.cs
@@ -121,6 +121,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -131,6 +132,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -141,6 +143,7 @@
             this.button3.TabIndex = 1;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -151,6 +154,7 @@
             this.button4.TabIndex = 1;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -161,6 +165,7 @@
             this.button5.TabIndex = 1;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -171,6 +176,7 @@
             this.button6.TabIndex = 1;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -181,6 +187,7 @@
             this.button7.TabIndex = 1;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -191,6 +198,7 @@
             this.button8.TabIndex = 1;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -201,6 +209,7 @@
             this.button9.TabIndex = 1;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -211,6 +220,7 @@
             this.button10.TabIndex = 1;
             this.button10.Text = "10";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // labelImages
             // 
--- 5/Form1.cs
+++ 5/Form1.cs
@@ -1,16 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace NeuralNetwork
 {
     public partial class Form1 : Form
     {
+        private readonly Dictionary<string, IEnumerable<Image>> SymbolExampleImages = new Dictionary<string, IEnumerable<Image>>();
+
         public Form1()
         {
             InitializeComponent();
             ResetImage();
+        }
+
+        // Нажатие на кнопку добавления тренировочных изображений для символа - открывает окно выбора файлов-изображений
+        private void button1_Click(object sender, EventArgs e) => AddExamples("1");
+        private void button2_Click(object sender, EventArgs e) => AddExamples("2");
+        private void button3_Click(object sender, EventArgs e) => AddExamples("3");
+        private void button4_Click(object sender, EventArgs e) => AddExamples("4");
+        private void button5_Click(object sender, EventArgs e) => AddExamples("5");
+        private void button6_Click(object sender, EventArgs e) => AddExamples("6");
+        private void button7_Click(object sender, EventArgs e) => AddExamples("7");
+        private void button8_Click(object sender, EventArgs e) => AddExamples("8");
+        private void button9_Click(object sender, EventArgs e) => AddExamples("9");
+        private void button10_Click(object sender, EventArgs e) => AddExamples("10");
+        private void AddExamples(string symbol)
+        {
+            var dialog = new OpenFileDialog {Multiselect = true, Filter = "Bitmap Image|*.bmp"};
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+            var bitmapFilePaths = dialog.FileNames;
+            var bitmaps = bitmapFilePaths.Select(Image.FromFile).ToArray();
+
+            if (SymbolExampleImages.ContainsKey(symbol)) SymbolExampleImages.Remove(symbol);
+            SymbolExampleImages.Add(symbol, bitmaps);
+
+            labelImages.Text = $"Изображений: {SymbolExampleImages.SelectMany(example => example.Value).Count()}";
         }
 
         // Рисование символа мышкой
